6a0362231c0cbe1cd77df5e7fecfb2553604afdf
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-04-16 10:52:57 -0700

    revamped calendar system

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-const monthYear = document.getElementById('monthYear');
-const dates = document.getElementById('dates');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+const monthName = document.getElementById('monthName');
+const yearName = document.getElementById('yearName');
+const dates = document.getElementById('dates');
+const addDay = document.createElement('button');
 
-let currentDate = new Date();
+var selectedMonth = 1;
 
-const updateCalendar = () => {
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
-    const firstDay = new Date(currentYear, currentMonth, 0);
-    const lastDay = new Date(currentYear, currentMonth + 1, 0);
-    const totalDays = lastDay.getDate();
-    const firstDayIndex = firstDay.getDay();
-    const lastDayIndex = lastDay.getDay();
-    const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
-    monthYear.textContent = monthYearString;
-    let dateHTML = '';
-    let dateInnerHTM
-    
-    for (let i = firstDayIndex; i > 0; i--) {
-        const prev = new Date(currentYear, currentMonth, 0 - 1 + 1);
-        dateHTML += `<div class="date inactive">${prev.getDate()}</div>`;
+function prevBtn() {
+    while (dates.firstChild) {
+        dates.removeChild(dates.firstChild);
     }
-
-    for (let i = 1; i <= totalDays; i++) {
-        const date = new Date(currentYear, currentMonth, i);
-        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
-        dateHTML += `<div class=date ${activeClass}">${i}</div>`;
- 
-    }
+    selectedMonth--;
+    myFunction();
 }
+
+function nextBtn() {
+    while (dates.firstChild) {
+        dates.removeChild(dates.firstChild);
+    }
+    selectedMonth++;
+    myFunction();
+}
+
+function myFunction() {
+    var today = new Date();
+    var numOfDays = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getDate();
+    var month = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).toLocaleString('default', { month: 'long' });
+    console.log(month + " has " + numOfDays + " days.");
+    var year = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getFullYear();
+
+
+
+    for (let i = 1  ; i <= numOfDays; i++) {
+        const addDay = document.createElement('button');
+        dates.appendChild(addDay);
+        addDay.innerText = i;
+    } 
+
+    monthName.innerText = month;
+    yearName.innerText = year;
+}
+
+myFunction();
+prev.addEventListener('click', prevBtn);
+next.addEventListener('click', nextBtn);
