18e129ec596d13aef40b7cc879e6ecdc07852caf
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-04-17 21:25:33 -0700

    added barebones mechanics to bind to-do list tasks to each calendar day.

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function nextBtn() {
 
 function myFunction() {
     var today = new Date();
-    console.log(today);
     var numOfDays = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getDate();
     var month = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).toLocaleString('default', { month: 'long' });
     //console.log(month + " has " + numOfDays + " days.");
     var year = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getFullYear();
     var firstDay = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 1-numOfDays).getDay();
-
+    let todaysDate = today.getDate();
+    
 
     for (let j = 0 ; j < firstDay; j++) {
         const firstDayOffset = document.createElement('div');
@@ -50,46 +50,63 @@ function myFunction() {
     monthName.innerText = month;
     yearName.innerText = year;
 
+    const addTask = document.getElementById('addTask');
+    const createTask = document.getElementById('createTask');
+    const taskName = document.getElementById('taskName');
+    const todo = document.getElementById('tasks');
 
+    // when page is initially loaded
     document.querySelectorAll('.day').forEach(item => {
         idYear = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getFullYear();
         idMonth = new Date(today.getFullYear(), today.getMonth()+selectedMonth, 0).getMonth();
         idDate = item.innerText;
-        fullId = "date-" + idYear + "-" + idMonth + "-" + idDate
+        fullId = "date-" + idYear + "-" + idMonth + "-" + idDate;
         item.id = fullId;
-        item.style.backgroundColor = 'white';
-        let itemDiv = document.createElement('div');
         if (item.innerText == new Date().getDate()) {
             item.style.backgroundColor = 'wheat';
+            item.classList.add('selected');
         }
-
+        else {
+            item.classList.remove('selected');
+            item.style.backgroundColor = 'white';
+        }
+        let selectedDate = document.querySelector('.selected');
+        
+        // when a date is clicked
         item.addEventListener('click', function() {
             document.querySelectorAll('.day').forEach(item => {
+                item.classList.remove('selected');
                 item.style.backgroundColor = 'white';
-                if (item.innerText == new Date().getDate()) {
-                    item.style.backgroundColor = 'wheat';
-                }
             });
-            itemDiv.id = item.id;
+            console.log(data);
+            item.style.backgroundColor = 'wheat';
+            item.classList.add('selected');
+            removeTaskFunc();
+            selectedDate.appendChild(data);
+            console.log(taskList);
+            data.classList.add(item.id);
             item.style.backgroundColor = 'burlywood';
-            itemDiv.innerText = taskList + " " + checkList;
-            console.log(itemDiv.id + " " + itemDiv.innerText);
 
-        })
-    })
+        });
+    });
 }
 
 myFunction();
 prev.addEventListener('click', prevBtn);
 next.addEventListener('click', nextBtn);
 
-const addTask = document.getElementById('addTask');
-const createTask = document.getElementById('createTask');
-const taskName = document.getElementById('taskName');
-const todo = document.getElementById('todo');
+let data = document.createElement('div');
+data.classList.add('hidden');
+data.classList.add(fullId);
+
 let taskNum = 0;
 let taskList = [];
 let checkList = [];
+function removeTaskFunc() {
+    document.querySelectorAll('.task').forEach(item => {
+        item.remove();
+    });
+}
 function addTaskFunc() {
     
 
@@ -99,6 +116,7 @@ function addTaskFunc() {
     taskChild.value = createTask.value;
     taskChild.setAttribute('readonly', true);
     createTask.value = '';
+    taskChild.classList.add('task');
     taskChild.classList.add('task-'+taskNum);
     taskList.push(taskChild.value);
     console.log(taskList);
@@ -107,35 +125,46 @@ function addTaskFunc() {
     checkbox.type = 'checkbox';
     todo.append(checkbox);
     checkbox.classList.add('checkbox');
+    checkbox.classList.add('task');
     checkbox.classList.add('task-'+taskNum);
     checkList.push(false);
+    selectedDate.removeChild(data);
+    data.innerText = taskList + " &+& " + checkList;
+    selectedDate.appendChild(data);
+    console.log(data);
 
     const remove = document.createElement('button');
     remove.innerText = 'Remove';
     todo.append(remove);
     remove.classList.add('removeBtn');
+    remove.classList.add('task');
     remove.classList.add('task-'+taskNum);
     taskNum++;
     
     document.querySelectorAll('.removeBtn').forEach(item => {
         item.addEventListener('click', function() {
-            let removeMe = item.classList[1];
+            let removeMe = item.classList[2];
             console.log(removeMe);
             document.querySelectorAll("."+removeMe).forEach(element => {
                 todo.removeChild(element);
                 let removeMeSplit = removeMe.split("-");
                 let removeMeNum = removeMeSplit[1];
+                let moveDownOne = removeMeNum;
                 console.log(removeMeNum);
                 taskList.splice(removeMeNum);
                 checkList.splice(removeMeNum);
-                console.log(taskList);
-            })
+            });
+            selectedDate.removeChild(data);
+            data.innerText = taskList + " &+& " + checkList;
+            selectedDate.appendChild(data);
+            console.log(data);
+            
         });
     });
 
     document.querySelectorAll('.checkbox').forEach(item => {
         item.addEventListener('change', function() {
-            let checkMe = item.classList[1];
+            let checkMe = item.classList[2];
             document.querySelectorAll("." +checkMe).forEach(element => {
                 if (item.checked) {
                     console.log(checkMe);
@@ -157,7 +186,11 @@ function addTaskFunc() {
                     checkList[checkMeNum] = false;
                     console.log(checkList);
                 }
-            })
+            });
+            selectedDate.removeChild(data);
+            data.innerText = taskList + " &+& " + checkList;
+            selectedDate.appendChild(data);
+            console.log(data);
         });
     });
 }
@@ -165,6 +198,5 @@ function addTaskFunc() {
 
 
 addTask.addEventListener('click', addTaskFunc);
-
 
 
