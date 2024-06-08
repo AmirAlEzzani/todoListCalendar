eedfbba5f6a59a706f3f6d5db6142045fc07aac3
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-06-07 21:28:36 -0700

    fixed bug where only index html would be served

--- index.js
+++ index.js
@@ -15,20 +15,24 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-// Serve the index.html file
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html by default
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Handle POST request for tasks
 app.post('/tasks', async (req, res) => {
     const { userid, date, tasklist, checklist } = req.body;
-    
+
     if (tasklist != null) {
         try {
             const task = await createTasks(userid, date, tasklist, checklist);
             res.status(201).send(task);
         } catch (error) {
+            console.error('Error creating tasks:', error);
             res.status(500).send('Failed to create tasks');
         }
     } else {
@@ -36,11 +40,12 @@ app.post('/tasks', async (req, res) => {
             let currentMonth = req.body.date;
             let userid = req.body.userid;
             let currentMonthSplit = currentMonth.split('-');
-            let currMonth = currentMonthSplit[0] + '-' + currentMonthSplit[1] + '-' + currentMonthSplit[2];
+            let currMonth = `${currentMonthSplit[0]}-${currentMonthSplit[1]}-${currentMonthSplit[2]}`;
             let tasks = await importTasks(userid, currMonth);
             taskStore.splice(0, 1, tasks);
             res.status(200).json(taskStore);
         } catch (error) {
+            console.error('Error importing tasks:', error);
             res.status(500).send('Failed to import tasks');
         }
     }
@@ -54,18 +59,19 @@ app.get('/tasks', async (req, res) => {
 // Handle DELETE request for tasks
 app.delete('/tasks', async (req, res) => {
     const { userid, date, tasklist, checklist } = req.body;
-    
+
     try {
         const task = await deleteTasks(userid, date, tasklist, checklist);
         res.status(201).send(task);
     } catch (error) {
+        console.error('Error deleting tasks:', error);
         res.status(500).send('Failed to delete tasks');
     }
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    console.error('Unexpected error:', err.stack);
     res.status(500).send('Something broke!');
 });
 
